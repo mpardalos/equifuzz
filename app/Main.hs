656630e3f8c7a908b8ce3768742a667c7fb3429b
Michalis Pardalos <me@mpardalos.com>
2023-02-01 18:02:16 +0000

    Print out some example Verilog using libverilog

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,28 @@
 module Main where
 
+import Data.Text (Text)
+import Data.Text qualified as T
+import Data.Text.IO qualified as T
+import Verismith.Verilog
+import Verismith.Verilog.AST
+
+boxed :: Text -> Text -> Text
+boxed title =
+  T.unlines
+    . (["┌───" <> title <> "───"] <>)
+    . (<> ["└──────"])
+    . map ("│ " <>)
+    . T.lines
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  T.putStrLn
+    . boxed "Example"
+    . genSource
+    $ ModDecl @()
+      "Example"
+      []
+      []
+      [ Decl Nothing (Port Wire False (Range 0 0) (Identifier "p1")) Nothing
+      ]
+      []
