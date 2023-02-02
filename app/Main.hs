a90e62ccaf359ff6041d0f861386ac1f667a67e0
Michalis Pardalos <me@mpardalos.com>
2023-02-02 13:45:00 +0000

    Generate a random Verilog module

--- app/Main.hs
+++ app/Main.hs
@@ -3,8 +3,9 @@ module Main where
 import Data.Text (Text)
 import Data.Text qualified as T
 import Data.Text.IO qualified as T
+import Hedgehog.Gen qualified as Hog
+import Verismith.Generate (randomMod)
 import Verismith.Verilog
-import Verismith.Verilog.AST
 
 boxed :: Text -> Text -> Text
 boxed title =
@@ -16,13 +17,8 @@ boxed title =
 
 main :: IO ()
 main = do
+  m :: ModDecl () <- Hog.sample (randomMod 2 4)
   T.putStrLn
     . boxed "Example"
     . genSource
-    $ ModDecl @()
-      "Example"
-      []
-      []
-      [ Decl Nothing (Port Wire False (Range 0 0) (Identifier "p1")) Nothing
-      ]
-      []
+    $ m
