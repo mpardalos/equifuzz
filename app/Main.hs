f0e21d3af7c2e901a5f4a71b74affd945e8b7cc8
Michalis Pardalos <me@mpardalos.com>
2023-02-14 14:10:18 +0000

    Add CLI subcommand to just generate

--- app/Main.hs
+++ app/Main.hs
@@ -3,8 +3,12 @@ module Main where
 import Brick.BChan qualified as B
 import Control.Concurrent (forkFinally)
 import Control.Monad (void)
+import Data.Text.IO qualified as T
 import Experiments
+import System.Environment (getArgs, getProgName)
 import TUI (AppEvent (..), runTUI)
+import Verismith.Verilog (genSource)
+import Text.Printf (printf)
 
 experimentThread :: B.BChan AppEvent -> IO ()
 experimentThread eventChan =
@@ -16,8 +20,29 @@ experimentThread eventChan =
           experimentThread eventChan
       )
 
-main :: IO ()
-main = do
+tuiMain :: IO ()
+tuiMain = do
   eventChan <- B.newBChan 10
   experimentThread eventChan
   runTUI eventChan
+
+genMain :: IO ()
+genMain = do
+  Experiment {design1, design2} <- mkNegativeExperiment
+  T.putStrLn (genSource design1)
+  putStrLn "---------"
+  T.putStrLn (genSource design2)
+
+main :: IO ()
+main = do
+  args <- getArgs
+  case args of
+    ["tui"] -> tuiMain
+    [] -> tuiMain
+    ["generate"] -> genMain
+    _ -> do
+      name <- getProgName
+      printf "Usage: %s [command]\n" name
+      printf "Commands:\n"
+      printf "  tui       : (default) Run the TUI\n"
+      printf "  generate  : Generate an experiment and write it to stdout\n"
