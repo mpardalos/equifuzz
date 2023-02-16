fe0a9fd871de9c45ffe16dcbac4218ddc61cb565
Michalis Pardalos <me@mpardalos.com>
2023-02-16 10:28:49 +0000

    Remove nix stuff

--- .dir-locals.el
+++
@@ -1,5 +0,0 @@
-((nil . ((eval . (add-to-list 'auto-mode-alist '("\\.v\\'" . verilog-mode)))
-;; It needs to be haskell-language-server, NOT haskell-language-server wrapper,
-;; because that could pull a globally-installed version of HLS instead of the
-;; nix-installed one that we want
-         (lsp-haskell-server-path . "haskell-language-server"))))
