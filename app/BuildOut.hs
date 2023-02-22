c2a9cf9e0846a7d969ac157c223890ee5e45a637
Michalis Pardalos <me@mpardalos.com>
2023-02-22 15:01:33 +0000

    Tag built-out expressions with the method used to generate them

--- app/BuildOut.hs
+++ app/BuildOut.hs
@@ -1,14 +1,41 @@
+{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE TypeFamilies #-}
 
 module BuildOut where
 
 import Control.Monad (guard)
+import Data.Data (Data)
+import Data.Generics.Uniplate.Data (universe)
+import Data.String (IsString)
+import Data.Text (Text)
+import Data.Text qualified as T
+import GHC.Generics (Generic)
 import Hedgehog (Gen)
 import Hedgehog.Gen qualified as Hog
 import Hedgehog.Range qualified as Hog.Range
 import Verismith.Verilog.AST
 
-type ExprPair = (Expr (), Expr ())
+newtype ExprSource = ExprSource Text
+  deriving newtype (IsString)
+  deriving stock (Generic, Data)
+
+instance Show ExprSource where
+  show (ExprSource txt) = T.unpack txt
+
+data BuildOut
+
+instance Annotation BuildOut where
+  type AnnExpr BuildOut = ExprSource
+  type AnnConstExpr BuildOut = ()
+  type AnnStatement BuildOut = ()
+  type AnnModItem BuildOut = ()
+  type AnnModDecl BuildOut = ()
+
+instance Default ExprSource where
+  def = ExprSource ""
+
+type ExprPair = (Expr BuildOut, Expr BuildOut)
 
 inequivalent :: Gen ExprPair
 inequivalent = Hog.choice [differentConstants]
@@ -18,7 +45,10 @@ differentConstants = do
   n1 <- Hog.int (Hog.Range.constant 0 255)
   n2 <- Hog.int (Hog.Range.constant 0 255)
   guard (n1 /= n2)
-  return (Number () (fromIntegral n1), Number () (fromIntegral n2))
+  return
+    ( Number "differentConstants" (fromIntegral n1),
+      Number "differentConstants" (fromIntegral n2)
+    )
 
 -- | Grow both expressions in a pair in an equivalent (but possibly not
 -- identical) way
@@ -34,27 +64,28 @@ grow pair = do
           bimapF or0 p
         ]
 
-deadExpression :: Gen (Expr ())
-deadExpression = Number () . fromIntegral <$> Hog.int (Hog.Range.constant 1 255)
+deadExpression :: Gen (Expr BuildOut)
+deadExpression = Number "dead" . fromIntegral <$> Hog.int (Hog.Range.constant 1 255)
 
 bimapF :: Applicative f => (a -> f a) -> (a, a) -> f (a, a)
 bimapF f (x, y) = (,) <$> f x <*> f y
 
-ifTrue :: Expr () -> Gen (Expr ())
+ifTrue :: Expr BuildOut -> Gen (Expr BuildOut)
 ifTrue e = do
-  condition <- Number () . fromIntegral <$> Hog.int (Hog.Range.constant 1 255)
+  condition <- Number "condT" . fromIntegral <$> Hog.int (Hog.Range.constant 1 255)
   falseBranch <- deadExpression
-  return (Cond () condition e falseBranch)
+  return (Cond "ifT" condition e falseBranch)
 
-ifFalse :: Expr () -> Gen (Expr ())
+ifFalse :: Expr BuildOut -> Gen (Expr BuildOut)
 ifFalse e = do
-  falseBranch <- deadExpression
-  return (Cond () (Number () 0) e falseBranch)
+  trueBranch <- deadExpression
+  return (Cond "ifF" (Number "condF" 0) trueBranch e)
 
-or0 :: Expr () -> Gen (Expr ())
-or0 e = pure (BinOp () e BinOr (Number () 0))
+or0 :: Expr BuildOut -> Gen (Expr BuildOut)
+or0 e = pure (BinOp "or0" e BinOr (Number "or0" 0))
 
-singleExprModule :: Identifier -> Expr () -> ModDecl ()
+
+singleExprModule :: Identifier -> Expr BuildOut -> ModDecl BuildOut
 singleExprModule name e =
   ModDecl
     { annotation = (),
