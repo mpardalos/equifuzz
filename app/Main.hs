25e577303f6451993124861da9598179b4345349
Michalis Pardalos <me@mpardalos.com>
2023-04-04 11:27:43 +0100

    Allow choosing generator for tui

--- app/Main.hs
+++ app/Main.hs
@@ -6,11 +6,12 @@ import Control.Monad (replicateM_, void)
 import Data.Text.IO qualified as T
 import Experiments
 import System.Environment (getArgs, getProgName)
+import System.Exit (exitFailure)
 import TUI (AppEvent (..), runTUI)
 import Text.Printf (printf)
 
-tuiMain :: IO ()
-tuiMain = do
+tuiMain :: IO Experiment -> IO ()
+tuiMain generator = do
   eventChan <- B.newBChan 20
   replicateM_ 10 $ experimentThread eventChan
   runTUI eventChan
@@ -19,35 +20,43 @@ tuiMain = do
     experimentThread eventChan =
       void $
         forkFinally
-          (experimentLoop mkSystemCVerilogExperiment runVCFormal (B.writeBChan eventChan . ExperimentProgress))
+          (experimentLoop generator runVCFormal (B.writeBChan eventChan . ExperimentProgress))
           (const $ experimentThread eventChan)
 
-genMain :: String -> IO ()
-genMain "systemc-verilog" = do
-  Experiment {design1, design2} <- mkSystemCVerilogExperiment
+genMain :: IO Experiment -> IO ()
+genMain generator = do
+  Experiment {design1, design2} <- generator
   T.putStrLn design1.source
   putStrLn "---------"
   T.putStrLn design2.source
-genMain "systemc-constant" = do
-  Experiment {design1,design2} <- mkSystemCConstantExperiment
-  T.putStrLn design1.source
-  putStrLn "---------"
-  T.putStrLn design2.source
-genMain t = printf "Unknown generate type: '%s'" t
+
+getExperimentGenerator :: String -> Maybe (IO Experiment)
+getExperimentGenerator "systemc-constant" = Just mkSystemCConstantExperiment
+getExperimentGenerator "systemc-verilog" = Just mkSystemCVerilogExperiment
+getExperimentGenerator _ = Nothing
+
+usageAndExit :: IO a
+usageAndExit = do
+  name <- getProgName
+  printf "Usage: %s <command> <generate-type>\n" name
+  printf "Commands:\n"
+  printf "  tui                Run the TUI\n"
+  printf "  generate           Generate an experiment and write it to stdout\n"
+  printf "Generate types:\n"
+  printf "  systemc-verilog\n"
+  printf "  systemc-constant\n"
+  exitFailure
 
 main :: IO ()
 main = do
   args <- getArgs
-  case args of
-    ["tui"] -> tuiMain
-    [] -> tuiMain
-    ["generate", genType] -> genMain genType
-    _ -> do
-      name <- getProgName
-      printf "Usage: %s [command]\n" name
-      printf "Commands:\n"
-      printf "  tui                (default) Run the TUI\n"
-      printf "  generate <type>    Generate an experiment and write it to stdout\n"
-      printf "Generate types:\n"
-      printf "  - systemc-verilog\n"
-      printf "  - systemc-constant\n"
+  (command, genType) <- case args of
+    [command, genType] -> pure (command, genType)
+    _ -> usageAndExit
+  generator <- case getExperimentGenerator genType of
+    Just generator -> pure generator
+    Nothing -> usageAndExit
+  case command of
+    "tui" -> tuiMain generator
+    "generate" -> genMain generator
+    _ -> usageAndExit
