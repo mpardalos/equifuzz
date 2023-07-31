248d9e50fbf6d88412a049407cfe436c0366e368
Michalis Pardalos <me@mpardalos.com>
2023-07-31 17:06:34 +0100

    Allow specifying gen-steps on the CLI

--- app/Main.hs
+++ app/Main.hs
@@ -15,14 +15,8 @@ import WebUI (runWebUI)
 #ifdef EVALUATION_VERSION
 import Control.Monad.Random (setStdGen, mkStdGen)
 #endif
-import Paths_equifuzz (version)
 import Data.Version
-
-genConfig :: GenConfig
-genConfig =
-  GenConfig
-    { growSteps = 30
-    }
+import Paths_equifuzz (version)
 
 main :: IO ()
 main = do
@@ -34,7 +28,7 @@ main = do
     Web orchestrationConfig -> do
       progressChan <- startRunners orchestrationConfig
       runWebUI progressChan
-    Generate -> do
+    Generate genConfig -> do
       Experiment {design, designDescription, comparisonValue} <- mkSystemCConstantExperiment genConfig
       T.putStrLn design.source
       putStrLn "---------"
@@ -48,12 +42,11 @@ main = do
       putStrLn (showVersion version <> " (full)")
 #endif
 
-
 --------------------------- CLI Parser -----------------------------------------
 
 data Command
   = Web OrchestrationConfig
-  | Generate
+  | Generate GenConfig
   | PrintVersion
 
 commandParser :: Opt.Parser Command
@@ -61,11 +54,11 @@ commandParser =
   Opt.hsubparser . mconcat $
     [ Opt.command "web" $
         Opt.info
-          (Web <$> orchestrationConfig)
+          (Web <$> orchestrationConfigOpts)
           (Opt.progDesc "Run the equifuzz Web UI, connected to a remote host"),
       Opt.command "generate" $
         Opt.info
-          (pure Generate)
+          (Generate <$> generateConfigOpts)
           (Opt.progDesc "Generate a sample of a generator"),
       Opt.command "version" $
         Opt.info
@@ -73,7 +66,7 @@ commandParser =
           (Opt.progDesc "Generate a sample of a generator")
     ]
   where
-    orchestrationConfig = do
+    orchestrationConfigOpts = do
       maxExperiments <-
         Opt.option Opt.auto . mconcat $
           [ Opt.long "max-experiments",
@@ -88,6 +81,7 @@ commandParser =
             Opt.help "Print experiment status to the console"
           ]
       runnerConfig <- runnerConfigOpts <|> testFlag
+      genConfig <- generateConfigOpts
       return
         OrchestrationConfig
           { runnerConfig,
@@ -104,6 +98,18 @@ commandParser =
         [ Opt.long "test",
           Opt.help "Show test data on the interface, don't run any fuzzing"
         ]
+
+    generateConfigOpts :: Opt.Parser GenConfig
+    generateConfigOpts =
+      GenConfig
+        <$> ( Opt.option Opt.auto . mconcat $
+                [ Opt.long "gen-steps",
+                  Opt.metavar "COUNT",
+                  Opt.help "Number of generation steps to use for each experiment",
+                  Opt.value 30,
+                  Opt.showDefault
+                ]
+            )
 
     runnerConfigOpts = do
       host <-
