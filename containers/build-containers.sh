b56b169818e999649d46f8cd28f24b9240c35817
Michalis Pardalos <me@mpardalos.com>
2023-08-11 18:24:59 +0300

    Add separate Containerfiles for building full or evaluation versions

---
+++ containers/build-containers.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+revision=$(git rev-parse --short HEAD)
+if [[ $(git status --short --untracked-files=no) ]]; then
+    revision="$revision-dirty"
+fi
+project_root=$(git rev-parse --show-toplevel)
+registry="ghcr.io/mpardalos"
+
+podman build \
+    --tag $registry/equifuzz-full:$revision \
+    --tag $registry/equifuzz-full:latest \
+    --file containers/Containerfile-full \
+    "$project_root"
+
+podman build \
+    --tag $registry/equifuzz-evaluation:$revision \
+    --tag $registry/equifuzz-evaluation:latest \
+    --file containers/Containerfile-evaluation \
+    "$project_root"
