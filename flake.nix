cc8caf3e9c0ba1c04b369cf7ec8ec7691b6e179b
Michalis Pardalos <me@mpardalos.com>
2024-04-18 16:14:42 +0100

    Add a nix flake

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "Fuzzer for equivalence checkers";
+
+  inputs = {
+    haskellNix.url = "github:input-output-hk/haskell.nix";
+    nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, haskellNix }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ] (system:
+    let
+      overlays = [ haskellNix.overlay
+        (final: prev: {
+          equifuzz =
+            final.haskell-nix.project' {
+              src = ./.;
+              compiler-nix-name = "ghc92";
+
+              shell.tools = {
+                cabal = "latest";
+              };
+
+              # Non-Haskell shell tools go here
+              shell.buildInputs = with pkgs; [
+                nixpkgs-fmt
+              ];
+            };
+        })
+      ];
+      pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
+      flake = pkgs.equifuzz.flake {};
+    in flake // {
+      packages.default = flake.packages."equifuzz:exe:equifuzz";
+    });
+
+  # --- Flake Local Nix Configuration ----------------------------
+  nixConfig = {
+    # This sets the flake to use the IOG nix cache.
+    # Nix should ask for permission before using it,
+    # but remove it here if you do not want it to.
+    extra-substituters = ["https://cache.iog.io"];
+    extra-trusted-public-keys = ["hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="];
+    allow-import-from-derivation = "true";
+  };
+}
