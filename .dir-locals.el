89af8ec1cead0b9bd332fe8a19c932595d94e003
Michalis Pardalos <me@mpardalos.com>
2024-04-18 16:14:42 +0100

    Add a nix flake

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((lsp-haskell-server-path . "haskell-language-server"))))
