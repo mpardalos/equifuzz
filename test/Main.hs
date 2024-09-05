28dbd1ba1fe35d7cd982b28fedebb57625d23f46
Michalis Pardalos <me@mpardalos.com>
2024-09-05 17:36:38 +0100

    Nicer output from test suite

--- test/Main.hs
+++ test/Main.hs
@@ -7,7 +7,7 @@ import Data.Map qualified as Map
 import Experiments (mkSystemCConstantExperiment)
 import GHC.Conc (numCapabilities)
 import GenSystemC (GenConfig (..), Reducible (..))
-import System.IO (hPutStrLn, stderr)
+import System.IO (hPutStrLn, stderr, hFlush)
 import Text.Printf (printf)
 import ToolRestrictions (noMods)
 
@@ -18,20 +18,29 @@ genConfig =
       mods = noMods
     }
 
-generateSomeExperiments :: Int -> IO ()
-generateSomeExperiments count = replicateM_ count $ do
-  experiment <- mkSystemCConstantExperiment genConfig
-  evaluate =<< experiment.value
-  -- | Just try a few arbitrary reductions
-  forM_ [(0, 10), (10, 20), (20, 29), (10, 12), (5, 25)] $ \bounds -> do
-    evaluate =<< (experiment.reductions Map.! bounds).value
+totalExperiments :: Int
+totalExperiments = 10
+
+reductions :: [(Int, Int)]
+reductions = [(0, 10), (10, 20), (20, 29), (10, 12), (5, 25)]
+
+progress :: Int -> Maybe Int -> String -> IO ()
+progress experiment mReduction message = do
+  hPutStrLn stderr (case mReduction of
+    Nothing -> printf "[%d/%d] %s" experiment totalExperiments message
+    Just reduction -> printf "[%d/%d] (%d/%d) %s" experiment totalExperiments reduction (length reductions) message)
 
 main :: IO ()
-main = do
-  threadCount <- getNumCapabilities
-
-  let totalExperiments = 10
-      experimentsPerThread = totalExperiments `div` threadCount
-
-  replicateM_ threadCount $
-    generateSomeExperiments experimentsPerThread
+main = forM_ [1..totalExperiments] $ \idx -> do
+  progress idx Nothing "Generating experiment"
+  experiment <- mkSystemCConstantExperiment genConfig
+  progress idx Nothing "Running experiment"
+  evaluate =<< experiment.value
+  progress idx Nothing "Experiment OK"
+  -- | Just try a few arbitrary reductions
+  forM_ (zip [1..] reductions) $ \(reductionIdx, bounds) -> do
+    progress idx (Just reductionIdx) "Generating reduction"
+    reduction <- evaluate (experiment.reductions Map.! bounds)
+    progress idx (Just reductionIdx) "Running reduction"
+    evaluate =<< reduction.value
+  progress idx Nothing "Reductions OK"
