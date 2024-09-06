47ceea24378c2a2e14f8155261e3dd186490c9d6
Michalis Pardalos <me@mpardalos.com>
2024-09-06 17:01:08 +0100

    Run tests in parallel, with a progress bar

--- test/Main.hs
+++ test/Main.hs
@@ -1,15 +1,22 @@
 module Main where
 
 import Control.Concurrent (getNumCapabilities)
-import Control.Exception (SomeException, evaluate)
+import Control.Exception (SomeException, evaluate, bracket_)
 import Control.Monad (forM_, replicateM_)
 import Data.Map qualified as Map
+import Data.Map (Map)
 import Experiments (mkSystemCConstantExperiment)
-import GHC.Conc (numCapabilities)
+import GHC.Conc (numCapabilities, atomically)
 import GenSystemC (GenConfig (..), Reducible (..))
-import System.IO (hPutStrLn, stderr, hFlush)
+import System.IO (hPutStrLn, stderr, hFlush, hPutStr)
 import Text.Printf (printf)
 import ToolRestrictions (noMods)
+import Control.Concurrent.Async (forConcurrently, forConcurrently_)
+import Control.Concurrent.MVar (newMVar, MVar, takeMVar, putMVar, modifyMVar, modifyMVar_, readMVar)
+import Control.Concurrent.STM.TSem
+import System.IO.Unsafe (unsafePerformIO)
+import Data.List (intercalate)
+import System.Console.ANSI (hClearLine, hSetCursorColumn)
 
 genConfig :: GenConfig
 genConfig =
@@ -24,23 +31,51 @@ totalExperiments = 10
 reductions :: [(Int, Int)]
 reductions = [(0, 10), (10, 20), (20, 29), (10, 12), (5, 25)]
 
-progress :: Int -> Maybe Int -> String -> IO ()
-progress experiment mReduction message = do
-  hPutStrLn stderr (case mReduction of
-    Nothing -> printf "[%d/%d] %s" experiment totalExperiments message
-    Just reduction -> printf "[%d/%d] (%d/%d) %s" experiment totalExperiments reduction (length reductions) message)
+totalRuns :: Int
+totalRuns = totalExperiments * (1 + length reductions)
+
+ioLock :: MVar ()
+{-# NOINLINE ioLock #-}
+ioLock = unsafePerformIO $ newMVar ()
+
+withIOLock :: IO a -> IO a
+withIOLock = bracket_ (takeMVar ioLock) (putMVar ioLock ())
+
+runsCompleteVar :: MVar Int
+{-# NOINLINE runsCompleteVar #-}
+runsCompleteVar = unsafePerformIO $ newMVar 0
+
+progressBar :: Int -> Int -> Int -> String
+progressBar totalSegments complete total =
+  let
+    fullSegments = (totalSegments * complete) `div` total
+    emptySegments = totalSegments - fullSegments
+  in
+    replicate fullSegments '=' <> replicate emptySegments ' '
+
+runCompleted :: IO ()
+runCompleted = do
+  modifyMVar_ runsCompleteVar (return . (1 +))
+  runsComplete <- readMVar runsCompleteVar
+  withIOLock $ do
+    hPutStr stderr "\r"
+    hPutStr stderr "["
+    hPutStr stderr (progressBar 50 runsComplete totalRuns)
+    hPutStr stderr "]"
+    hPutStr stderr (printf "[%3d/%-3d]" runsComplete totalRuns)
 
 main :: IO ()
-main = forM_ [1..totalExperiments] $ \idx -> do
-  progress idx Nothing "Generating experiment"
-  experiment <- mkSystemCConstantExperiment genConfig
-  progress idx Nothing "Running experiment"
-  evaluate =<< experiment.value
-  progress idx Nothing "Experiment OK"
-  -- | Just try a few arbitrary reductions
-  forM_ (zip [1..] reductions) $ \(reductionIdx, bounds) -> do
-    progress idx (Just reductionIdx) "Generating reduction"
-    reduction <- evaluate (experiment.reductions Map.! bounds)
-    progress idx (Just reductionIdx) "Running reduction"
-    evaluate =<< reduction.value
-  progress idx Nothing "Reductions OK"
+main = do
+  capabilities <- getNumCapabilities
+  count <- atomically $ newTSem (fromIntegral capabilities)
+  forConcurrently_ [1::Int .. totalExperiments] $ \_experimentIdx -> do
+    atomically $ waitTSem count
+    reducible <- mkSystemCConstantExperiment genConfig
+    _experiment <- evaluate =<< reducible.value
+    runCompleted
+    forM_ (zip [1::Int ..] reductions) $ \(_reductionIdx, bounds) -> do
+      reducible' <- evaluate (reducible.reductions Map.! bounds)
+      _experiment <- evaluate =<< reducible'.value
+      runCompleted
+    atomically $ signalTSem count
+  hPutStrLn stderr ""
