12260ecd003ee7494955bc6be19097b34e18123e
Michalis Pardalos <me@mpardalos.com>
2025-09-05 16:44:06 +0100

    Add flags to equifuzz-gen

--- exe/equifuzz-gen.hs
+++ exe/equifuzz-gen.hs
@@ -1,9 +1,11 @@
-{-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE ApplicativeDo #-}
+{-# LANGUAGE RecordWildCards #-}
 
 module Main where
 
-import Control.Monad (replicateM_)
+import CLI (readFecType, runOptParse)
+import Data.Aeson (encode)
+import Data.ByteString.Lazy.Char8 qualified as LBS
 import Data.Text.IO qualified as T
 import Experiments (
   Experiment (..),
@@ -11,34 +13,77 @@ import Experiments (
   showEvaluation,
  )
 import GenSystemC (GenConfig (..))
-import Safe (readMay)
-import System.Environment (getArgs)
-import System.Exit (die)
+import Optics (view)
+import Options.Applicative qualified as Opt
 import ToolRestrictions (noMods)
 
-genConfig :: GenConfig
-genConfig =
-  GenConfig
-    { growSteps = 10
-    , genMods = noMods
-    , evaluations = 5
-    }
+data Args
+  = Args
+  { jsonMode :: Bool
+  , genConfig :: GenConfig
+  }
+
+argParser :: Opt.Parser Args
+argParser = do
+  jsonMode <-
+    Opt.switch . mconcat $
+      [ Opt.long "json"
+      , Opt.help "Output experiment in JSON format"
+      ]
+
+  genMods <-
+    Opt.option (view #mods <$> readFecType) . mconcat $
+      [ Opt.long "fec-type"
+      , Opt.metavar "TYPE"
+      , Opt.help "What FEC type to generate for (vcf|catapult|jasper)"
+      , Opt.value noMods
+      ]
+
+  evaluations <-
+    Opt.option Opt.auto . mconcat $
+      [ Opt.long "evaluations"
+      , Opt.metavar "COUNT"
+      , Opt.help "How many values to evaluate the generated module at"
+      , Opt.value 5
+      , Opt.showDefault
+      ]
+
+  growSteps <-
+    Opt.option Opt.auto . mconcat $
+      [ Opt.long "grow-steps"
+      , Opt.metavar "COUNT"
+      , Opt.help "Size of the generated program"
+      , Opt.value 30
+      , Opt.showDefault
+      ]
+
+  return
+    Args
+      { genConfig =
+          GenConfig
+            { growSteps
+            , genMods
+            , evaluations
+            }
+      , ..
+      }
 
 main :: IO ()
 main = do
-  count <-
-    getArgs >>= \case
-      [readMay -> Just count] -> pure count
-      [] -> pure 1
-      _ -> die "Invalid arguments"
+  Args{genConfig, jsonMode} <- runOptParse "Generate a program with equifuzz" argParser
 
-  replicateM_ count $ do
-    Experiment{scSignature, scDesign, verilogDesign, knownEvaluations} <-
-      genSystemCConstantExperiment genConfig
-    T.putStrLn scDesign
-    putStrLn "---------"
-    T.putStrLn verilogDesign
-    putStrLn "---------"
-    mapM_
-      (T.putStrLn . showEvaluation scSignature)
-      knownEvaluations
+  experiment <- genSystemCConstantExperiment genConfig
+
+  if jsonMode
+    then LBS.putStrLn (encode experiment)
+    else outputExperiment experiment
+
+outputExperiment :: Experiment -> IO ()
+outputExperiment Experiment{scSignature, scDesign, verilogDesign, knownEvaluations} = do
+  T.putStrLn scDesign
+  putStrLn "---------"
+  T.putStrLn verilogDesign
+  putStrLn "---------"
+  mapM_
+    (T.putStrLn . showEvaluation scSignature)
+    knownEvaluations
